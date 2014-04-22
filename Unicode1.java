4119e249c480c45429f2ca72abe19bccf1dd41e3
Yukiko Yasuda <yasuday@jp.ibm.com>
2014-04-22 01:52:59 -0400

    承诺３

---
+++ Unicode1.java
@@ -0,0 +1,16 @@
+﻿
+public class Unicode1 {
+  public static void main(String[] args) {
+
+    // 文字「测」
+    char c = '测';
+    
+    // 取得Unicode code
+    int code = (int)c;
+    
+    System.out.println(code);
+
+    System.out.println(Integer.toHexString(code));
+    
+  }
+}
